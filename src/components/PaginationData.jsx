df8979122883023b5caa0d8a7d8612a509cf4589
purplecapguy <ejikegodfrey09025525079@gmail.com>
2024-06-24 15:39:16 -0700

    second commit

---
+++ src/components/PaginationData.jsx
@@ -0,0 +1,50 @@
+export const pageData = [
+    {
+        id: 1,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/nvcowkmuonk6ctt0w5m3',
+    },
+    {
+        id: 2,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/nvcowkmuonk6ctt0w5m3',
+    },
+    {
+        id: 3,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/nvcowkmuonk6ctt0w5m3',
+    },
+    {
+        id: 4,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/nvcowkmuonk6ctt0w5m3',
+    },
+    {
+        id: 5,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/m7uchxi1v1co95qhqgiz',
+    },
+    {
+        id: 6,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/m7uchxi1v1co95qhqgiz',
+    },
+    {
+        id: 7,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/m7uchxi1v1co95qhqgiz',
+    },
+    {
+        id: 8,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/m7uchxi1v1co95qhqgiz',
+    },
+    {
+        id: 9,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/k0wesohemosj2saw0zqh',
+    },
+    {
+        id: 10,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/k0wesohemosj2saw0zqh',
+    },
+    {
+        id: 11,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/k0wesohemosj2saw0zqh',
+    },
+    {
+        id: 12,
+        src: 'https://res.cloudinary.com/dneshezhq/image/upload/f_auto,q_auto/v1/ecommerce/our%20product/k0wesohemosj2saw0zqh',
+    },
+]
